ab4c139e9d9bb9b350868594acf2f23c78e3ca25
heshan <heshansandakelum2018@gmail.com>
2026-01-04 20:34:28 +0530

    setup course material service

---
+++ service/courseMaterialService.js
@@ -0,0 +1,19 @@
+async function getAllCourseMaterialls(params) {
+    
+}
+
+async function addCourseMaterial(params) {
+    
+}
+
+async function deleteCourseMaterial(params) {
+    
+}
+
+async function updateCourseMaterials(params) {
+    
+}
+
+module.exports={AllCourseMaterialls,addCourseMaterial,updateCourseMaterials,deleteCourseMaterial}
+
+ 
