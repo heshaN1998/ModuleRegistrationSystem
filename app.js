c043f4e9719a79d757648eaf27729934ad192763
heshan <heshansandakelum2018@gmail.com>
2026-01-19 18:58:53 +0530

    start dbConnect  part

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require("express")
 const app= express()
 const PORT = 3500
 const contextPathWithAPIVersion="/courseregis/api/v1"
+const courseMaterialRoutes =require("./routes/courseMaterialRoutes")
+const mongoose = require("mongoose")
 
-//
 app.get('${contextPathWithAPIVersion}/heartbeat',(req,res)=>{
     res.send("Course Regis Pro running")
 });
 
-app.listen(POR,()=>{
-    console.log("sERVER sTARTED IN PORT : ",PORT)
 
-})
+//DB Connect
+mongoose.connect("mongodb://localhost:27017/courseRegis110Pro",{userNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>console.log("connected to mongoDB"))
+    .catch(err=>console.error("failed to connect mongoDB",err))
+app.listen(POR,()=>{
+    console.log("Server Started IN PORT : ",PORT)
+
+});
 
